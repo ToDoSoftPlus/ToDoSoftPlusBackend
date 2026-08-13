ba1df65877f594783d055a54f30d8501309d9be0
QusDev <qusdeveloper@gmail.com>
2026-08-13 21:21:50 +0300

    add docker compose file

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Add reading a local appsettings file for development environment
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.Local.json",
+        optional: true,
+        reloadOnChange: true);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
