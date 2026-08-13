16d81ae76a0d2f6546b62d382af1214e94e0417d
QusDev <qusdeveloper@gmail.com>
2026-08-13 20:38:34 +0300

    add entity configurations

---
+++ Infrastructure/EntityConfigurations/ToDoCategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigurations
+{
+    public class ToDoCategoryConfiguration : IEntityTypeConfiguration<ToDoCategoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<ToDoCategoryEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id);
+
+            builder
+                .HasOne(x => x.User)
+                .WithMany(x => x.Categories)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasMany(x => x.ToDoItemsList)
+                .WithOne();
+        }
+    }
+}
---
+++ Infrastructure/EntityConfigurations/ToDoItemConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigurations
+{
+    public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItemEntity>
+    {
+        public void Configure(EntityTypeBuilder<ToDoItemEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id);
+
+            builder
+                .HasOne(x => x.User)
+                .WithMany(x => x.Items)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
