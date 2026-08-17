a6b563f3406ad16b394b82e0b9bd3f8a010c2525
QusDev <qusdeveloper@gmail.com>
2026-08-17 20:16:33 +0300

    add custom validation exception, add exception middleware, remove auto validation

---
+++ Application/Exceptions/CustomValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Exceptions
+{
+    public class CustomValidationException : Exception
+    {
+        public IReadOnlyDictionary<string, string[]> Errors { get; }
+
+        public CustomValidationException(IDictionary<string, string[]> errors) : base("One or more validation errors occurred.")
+        {
+            Errors = new Dictionary<string, string[]>(errors);
+        }
+    }
+}
---
+++ Application/FluentValidators/Identity/RegisterDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.Identity;
+using FluentValidation;
+
+namespace Application.FluentValidators.Identity
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.UserName)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("Username is required.")
+                .MinimumLength(3).WithMessage("Username must exceed 3 characters.");
+
+            RuleFor(x => x.Email)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("Email is required.")
+                .EmailAddress();
+        }
+    }
+}
